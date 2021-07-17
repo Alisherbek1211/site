3dc4844704f08cbdd57a3158a325d839e1170a67
Alisherbek <79314583+Alisherbek1211@users.noreply.github.com>
2021-07-17 20:18:44 +0500

    create models

--- store/models.py
+++ store/models.py
@@ -1,3 +1,28 @@
 from django.db import models
+from django.db.models.fields import CharField
 
-# Create your models here.
+
+class User(models.Model):
+    firstname = models.CharField(max_length=255)
+    lastname = models.CharField(max_length=255)
+    email = models.CharField(max_length=255)
+    phono_number = models.IntegerField()
+    password = models.CharField(max_length=255)
+    is_admin = models.BooleanField(default=False)
+    is_staff = models.BooleanField(default=True)
+    is_superuser = models.BooleanField(default=False)
+
+class Product(models.Model):
+    name = models.CharField(max_length=255)
+    price = models.FloatField()
+    stock = models.IntegerField()
+    product_description = models.TextField(max_length=255)
+    rating = models.FloatField()
+    cupon = models.FloatField()
+    created_at = models.DateTimeField()
+    updated_at = models.DateTimeField()
+
+class Category(models.Model):
+    name = models.CharField(max_length=255)
+    created_at = models.DateTimeField()
+    updated_at = models.DateTimeField()
