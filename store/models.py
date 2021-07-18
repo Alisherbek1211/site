857c9fc39741254a85cbba504638c4a28388fdf8
Alisherbek <79314583+Alisherbek1211@users.noreply.github.com>
2021-07-18 08:51:09 +0500

    update models

--- store/models.py
+++ store/models.py
@@ -1,28 +1,31 @@
 from django.db import models
-from django.db.models.fields import CharField
 
 
 class User(models.Model):
     firstname = models.CharField(max_length=255)
     lastname = models.CharField(max_length=255)
     email = models.CharField(max_length=255)
-    phono_number = models.IntegerField()
+    phone_number = models.CharField(max_length=255,null=True)
     password = models.CharField(max_length=255)
     is_admin = models.BooleanField(default=False)
     is_staff = models.BooleanField(default=True)
     is_superuser = models.BooleanField(default=False)
 
+
 class Product(models.Model):
     name = models.CharField(max_length=255)
-    price = models.FloatField()
+    price = models.FloatField() 
     stock = models.IntegerField()
-    product_description = models.TextField(max_length=255)
+    product_description = models.TextField()
     rating = models.FloatField()
     cupon = models.FloatField()
-    created_at = models.DateTimeField()
-    updated_at = models.DateTimeField()
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
 
 class Category(models.Model):
     name = models.CharField(max_length=255)
-    created_at = models.DateTimeField()
-    updated_at = models.DateTimeField()
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
