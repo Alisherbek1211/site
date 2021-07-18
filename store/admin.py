b9aab46e80ccb963842b87ba61fe82e49fbe457b
Alisherbek <79314583+Alisherbek1211@users.noreply.github.com>
2021-07-18 16:55:34 +0500

    Update admin panel

--- store/admin.py
+++ store/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
+from .models import *
 
-# Register your models here.
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ["name", "created_at", "updated_at"]
+    list_display_links = ["name", "created_at", "updated_at"]
+    search_fields = ["name", "created_at", "updated_at"]
+
+class UserAdmin(admin.ModelAdmin):
+    list_display = ["firstname","lastname","email","phone_number","password","is_admin","is_staff","is_superuser"]
+    # list_display_links = ["firstname","lastname","email","phone_number","password"]
+    search_fields = ["firstname","lastname","email","phone_number","password"]
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ["name","price","stock","product_description","rating","category","created_at","updated_at","icon"]
+    list_display_links = ["name","price","stock","product_description","rating","category","created_at","updated_at","icon"]
+    search_fields = ["name","price","stock","product_description","rating","created_at","updated_at","icon"]
+
+admin.site.register(User,UserAdmin)
+admin.site.register(Category,CategoryAdmin)
+admin.site.register(Product,ProductAdmin)
--- store/models.py
+++ store/models.py
@@ -2,30 +2,43 @@ from django.db import models
 
 
 class User(models.Model):
-    firstname = models.CharField(max_length=255)
-    lastname = models.CharField(max_length=255)
-    email = models.CharField(max_length=255)
-    phone_number = models.CharField(max_length=255,null=True)
-    password = models.CharField(max_length=255)
-    is_admin = models.BooleanField(default=False)
-    is_staff = models.BooleanField(default=True)
-    is_superuser = models.BooleanField(default=False)
-
-
-class Product(models.Model):
-    name = models.CharField(max_length=255)
-    price = models.FloatField() 
-    stock = models.IntegerField()
-    product_description = models.TextField()
-    rating = models.FloatField()
-    cupon = models.FloatField()
-
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
+    firstname = models.CharField(verbose_name="Firstname",max_length=255)
+    lastname = models.CharField(verbose_name="Lastname",max_length=255,null=True)
+    email = models.CharField(verbose_name="User Password",max_length=255)
+    phone_number = models.CharField(verbose_name="Phone Number",max_length=20,null=True)
+    password = models.CharField(verbose_name="Password",max_length=255)
+    is_admin = models.BooleanField(verbose_name="Admin",default=False)
+    is_staff = models.BooleanField(verbose_name="Staff",default=True)
+    is_superuser = models.BooleanField(verbose_name="Superuser",default=False)
+    
+    def __str__(self):
+        return self.name
 
 
 class Category(models.Model):
-    name = models.CharField(max_length=255)
+    class Meta:
+        verbose_name = "Category"
+        verbose_name_plural = "Categories"
+        
+    name = models.CharField(verbose_name="Category names" ,max_length=255)
 
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
+    created_at = models.DateTimeField(verbose_name="Date of creation", auto_now_add=True)
+    updated_at = models.DateTimeField(verbose_name="Modified date",auto_now=True)
+
+    def __str__(self):
+        return self.name
+
+class Product(models.Model):
+    name = models.CharField(verbose_name="Products name",max_length=255)
+    price = models.FloatField(verbose_name="The price of the product") 
+    stock = models.IntegerField(verbose_name="Stock",default=1)
+    product_description = models.TextField(verbose_name="Description")
+    rating = models.FloatField(verbose_name="Rate")
+    category = models.ForeignKey(Category,on_delete=models.CASCADE,null=True)
+    icon = models.ImageField(verbose_name="images",upload_to="image/", height_field=None, width_field=None, max_length=100,null=True)
+    
+    created_at = models.DateTimeField(verbose_name="Date of creation",auto_now_add=True)
+    updated_at = models.DateTimeField(verbose_name="Modified date",auto_now=True)
+    
+    def __str__(self):
+        return self.name
